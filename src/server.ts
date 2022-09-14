d647ca5eeec3b23468d06582cce64632073a766b
Igor Lira <igorlirap@gmail.com>
2022-09-14 20:43:01 -0300

    add sqlite driver and create game / ads tables

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,6 @@ import express from "express";
 const app = express();
 
 app.get("/ads", (req, res) => {
-  console.log("acessou Ads!");
   res.status(200).json({
     message: "Hello World",
   });
