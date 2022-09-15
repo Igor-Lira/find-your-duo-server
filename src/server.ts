4f37966b3807ec7708448f31b1c29a1488b49835
Igor Lira <igorlirap@gmail.com>
2022-09-14 21:44:11 -0300

    add routes to get games and ads, and post ads by connecting with db

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,93 @@
 import express from "express";
+import cors from "cors";
+import { PrismaClient } from "@prisma/client";
+import convertHourStringToMinutes, {
+  convertMinutesToHourString,
+} from "./utils/convert";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
 
-app.get("/ads", (req, res) => {
-  res.status(200).json({
-    message: "Hello World",
+const prisma = new PrismaClient({
+  log: ["query"],
+});
+
+app.get("/games/:id/ads", async (req, res) => {
+  const gameId = req.params.id;
+  const ads = await prisma.ad.findMany({
+    select: {
+      id: true,
+      name: true,
+      weekDays: true,
+      useVoiceChannel: true,
+      yearsPlaying: true,
+      hoursStart: true,
+      hoursEnd: true,
+    },
+    where: {
+      gameId: gameId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
   });
+  return res.json(
+    ads.map((ad) => {
+      return {
+        ...ad,
+        weekDays: ad.weekDays.split(","),
+        hoursStart: convertMinutesToHourString(ad.hoursStart),
+        hoursEnd: convertMinutesToHourString(ad.hoursEnd),
+      };
+    })
+  );
+});
+
+app.get("/games", async (req, res) => {
+  const games = await prisma.game.findMany({
+    include: {
+      _count: {
+        select: {
+          ads: true,
+        },
+      },
+    },
+  });
+  return res.json(games);
+});
+
+app.get("/ads/:id/discord", async (req, res) => {
+  const adId = req.params.id;
+  const ad = await prisma.ad.findUniqueOrThrow({
+    select: {
+      discord: true,
+    },
+    where: {
+      id: adId,
+    },
+  });
+  return res.json({
+    discord: ad.discord,
+  });
+});
+
+app.post("/games/:id/ads/", async (req, res) => {
+  const gameId = req.params.id;
+  const body: any = req.body;
+  const ad = await prisma.ad.create({
+    data: {
+      gameId,
+      name: body.name,
+      yearsPlaying: body.yearsPlaying,
+      discord: body.discord,
+      weekDays: body.weekDays.join(","),
+      hoursStart: convertHourStringToMinutes(body.hoursStart),
+      hoursEnd: convertHourStringToMinutes(body.hoursEnd),
+      useVoiceChannel: body.useVoiceChannel,
+    },
+  });
+  return res.status(201).json(ad);
 });
 
 app.listen(3333);
